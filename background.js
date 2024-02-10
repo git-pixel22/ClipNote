022feb8a6e797c05c934c012d3eedf27304d6e75
git-pixel22 <workemail.rajat@gmail.com>
2024-02-10 12:36:49 +0530

    rehaul

--- background.js
+++ background.js
@@ -1,12 +1,27 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+console.log("background.js activated")
+
+
+// this function sends the tabId, videoId, and a response function to content script
+function sendMessageToContentScript(tabId, url){
+
+  if(url && url.includes("youtube.com/watch")){
+
+    const queryParameters = url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    console.log(urlParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v")
+    }, function(response){
+      if(chrome.runtime.lastError){
+        console.log(`Error: ${chrome.runtime.lastError.message}`)
+      }else{
+        console.log(`Received response: ${response}`);
+      }
+    })
+
+  }
+}
+
